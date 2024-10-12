24a8bb82ee5003ea7655a584ba95d1a15a01ebb6
kooroush <sirusingapoora@gmail.com>
2024-10-12 20:15:28 +0330

    fix: release fix

--- release.config.js
+++ release.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  branches: ["main"],
+  branches: ['main'],
+  repositoryUrl: 'https://github.com/kooroushpsnd/Crypto-App-Back.git',
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
-    "@semantic-release/git"
-  ]
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    '@semantic-release/git',
+  ],
 };
